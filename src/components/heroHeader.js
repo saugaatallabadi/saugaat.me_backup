58b8f277d1362f140540105d2aeb1f22bda4e0a9
Saugaat <saugaatallabadi@gmail.com>
2020-11-30 17:50:08 +0400

    testing for image not working

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,8 +21,10 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='https://www.linkedin.com/in/saugaatallabadi/' className="button -primary">LinkedIn &rarr;</Link>
-        <Link to='https://github.com/saugaatallabadi/' className="button -primary">GitHub &rarr;</Link>
+        {/* <Link href='https://www.linkedin.com/in/saugaatallabadi/' className="button -primary">LinkedIn &rarr;</Link> */}
+        {/* <Link to='https://github.com/saugaatallabadi/' className="button -primary">GitHub &rarr;</Link> */}
+        <a href='https://www.linkedin.com/in/saugaatallabadi/' className="button -primary">LinkedIn &rarr;</a>
+        <a href='https://github.com/saugaatallabadi/' className="button -primary">GitHub &rarr;</a>
         <Link to='/contact' className="button -secondary">Get in touch &rarr;</Link>
       </div>
     )}
